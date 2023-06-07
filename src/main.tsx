d72738075f00e202440aac7ea97dbf40b5103dad
Adriaaann <adriansouza04@gmail.com>
2023-06-07 15:32:21 -0300

    clean up

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { WrappedApp } from './App';
-import store from './app/store';
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
    <React.StrictMode>
-      <Provider store={store}>
-         <WrappedApp />
-      </Provider>
+      <WrappedApp />
    </React.StrictMode>
 );
